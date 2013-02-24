e8628beaaeaed81fcb5be424f5111da87933813c
catshirt <caden@catshirt.net>
2013-02-24 01:03:28 -0500

    no more scaffolding

--- src/js/config.js
+++ src/js/config.js
@@ -1,41 +1,29 @@
 require.config({
-  name: 'index',
+  name: 'brainy-boilerplate',
   deps: ['main'],
   baseUrl: 'js',
 
   paths: {
     'jquery': 'lib/jquery',
     'underscore': 'lib/underscore',
-    'backbone': 'lib/backbone',
-    'backbone.subscribe': 'lib/backbone.subscribe',
-    'backbone.layout': 'lib/backbone.layout',
-    'socket.io': 'lib/socket.io',
-    'handlebars': 'lib/handlebars',
-    'hbs': 'lib/require.handlebars'
+    'backbone': 'lib/backbone'
   },
 
   shim: {
+
     'jquery': {
       exports: 'jQuery'
     },
+
     'underscore': {
       exports: '_'
     },
+
     'backbone': {
       deps: ['jquery', 'underscore'],
       exports: 'Backbone'
-    },
-    'backbone.subscribe': {
-      deps: ['jquery', 'backbone', 'socket.io'],
-      exports: 'Backbone.subscribe'
-    },
-    'backbone.layout': {
-      deps: ['backbone'],
-      exports: 'Backbone.Layout'
     }
-  },
 
-  hbs: {
-    disableI18n: true
   }
+
 });
