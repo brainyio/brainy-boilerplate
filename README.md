33ae442fbf75357dfadb31157be169c79033da90
catshirt <caden@catshirt.net>
2013-03-02 15:42:32 -0500

    adding backbone.sync default

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # brainy-boilerplate
 
-a dead simple client side boilerplate implementing Backbone and RequireJS. this repository can be used as a stand alone boilerplate, and is intended to show the base requirements for a brainy project.
+a client side boilerplate implementing Backbone and RequireJS.
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,19 @@ require([
   'backbone'
 ], function(Backbone) {
 
+  // override Backbone.sync because brainy-server
+  // runs the API on a different port
+  var sync = Backbone.sync;
+  Backbone.sync = function(method, model, options) {
+    options = options || {};
+    var protocol = location.protocol,
+      host = location.hostname,
+      port = 8080,
+      path = _.result(model, 'url');
+    options.url = prototcol + '//' + host + ':' + port + path;
+    sync.apply(null, [method, model, options]);
+  };
+
   console.log('hello world', Backbone);
 
 });
